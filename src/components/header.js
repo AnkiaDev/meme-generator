61f2bc524eba3af3011679613df66ea04f18ffe2
AnkiaDev <ankiadev@gmail.com>
2019-11-05 16:53:58 +0100

    create header and prepare state and lifecycle for meme generator

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header>
+      <img
+        src="http://pngimg.com/uploads/trollface/trollface_PNG26.png"
+        alt=""
+      />
+      <p>Meme Generator</p>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/memeGenerator.js
+++ src/components/memeGenerator.js
@@ -3,10 +3,23 @@ import React, { Component } from "react";
 class MemeGenerator extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+        topText: "",
+        botText: "",
+        randomImg: "http://i.imgflip.com/1bij.jpg",
+        allMemeImgs: ""
+    };
   }
+
+ componentDidMount() {
+     fetch("http://i.imgflip.com/get_memes")
+     .then(res => res.json())
+     .then(data => this.setState({allMemeImgs: data}))
+ }
   render() {
-    return <div></div>;
+    return <div>
+    {console.log(this.state.allMemeImgs)}
+    </div>;
   }
 }
 
